9840317aa2ab8e1cfdc674e77df3c71d60e26d68
Eugene Marinelli <eugeneiiim@gmail.com>
2014-02-13 21:12:50 -0800

    Revert "Nitpick reduce code"
    This reverts commit 40f909af5efc4a8e5941da49a966fb727d5a9b57.

--- tasks/hub.js
+++ tasks/hub.js
@@ -26,8 +26,9 @@ module.exports = function(grunt) {
     // Get process.argv options without grunt.cli.tasks to pass to child processes
     var cliArgs = _.without.apply(null, [[].slice.call(process.argv, 2)].concat(grunt.cli.tasks));
     // Get it's own gruntfile
-    var ownGruntfile += grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
-    ownGruntfile = path.resolve(process.cwd(), ownGruntfile || '');
+    var ownGruntfile = "";
+    ownGruntfile += grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
+    ownGruntfile = path.resolve(process.cwd(), ownGruntfile);
 
     var lastGruntFileWritten;
     function write(gruntfile, buf, isError) {
